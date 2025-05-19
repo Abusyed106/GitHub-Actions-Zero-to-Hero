c93ecc08ce90c1218c0b2c8456a495e61ebc59a0
Abusyed106 <Abushaik7657@gmail.com>
2025-05-19 14:12:15 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,9 @@
 # app.py
 # This is a test commit
 # This is also test commmit
+# removed variable be for testing advance test commit
 def add(a, b):
-    return a + b
+    return a + 
 
 def test_add():
     assert add(1, 2) == 3
